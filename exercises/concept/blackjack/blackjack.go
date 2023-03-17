4bcb332140d9b0edb0a98996b3ea2225c4ed6036
chenkun <kun.chen@yintech.cn>
2023-03-17 14:53:40 +0800

    Feat: blackjack

--- exercises/concept/blackjack/blackjack.go
+++ exercises/concept/blackjack/blackjack.go
@@ -2,11 +2,41 @@ package blackjack
 
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
-	panic("Please implement the ParseCard function")
+	cards := map[string]int{
+		"ace":   11,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+		"ten":   10,
+		"jack":  10,
+		"queen": 10,
+		"king":  10,
+	}
+	return cards[card]
 }
 
 // FirstTurn returns the decision for the first turn, given two cards of the
 // player and one card of the dealer.
 func FirstTurn(card1, card2, dealerCard string) string {
-	panic("Please implement the FirstTurn function")
+	handScore := ParseCard(card1) + ParseCard(card2)
+	dealerScore := ParseCard(dealerCard)
+	switch {
+	case handScore == 22:
+		return "P"
+	case handScore == 21:
+		if dealerScore < 10 {
+			return "W"
+		} else {
+			return "S"
+		}
+	case handScore >= 17 || handScore >= 12 && dealerScore < 7:
+		return "S"
+	default:
+		return "H"
+	}
 }
