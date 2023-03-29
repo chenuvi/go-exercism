112a297cba4ec62008ec77e09bdc90d7971254b4
chenkun <kun.chen@yintech.cn>
2023-03-29 12:53:09 +0800

    for array

--- exercises/concept/bird-watcher/bird_watcher.go
+++ exercises/concept/bird-watcher/bird_watcher.go
@@ -3,17 +3,28 @@ package birdwatcher
 // TotalBirdCount return the total bird count by summing
 // the individual day's counts.
 func TotalBirdCount(birdsPerDay []int) int {
-	panic("Please implement the TotalBirdCount() function")
+	total := 0
+	for _, val := range birdsPerDay {
+		total += val
+	}
+	return total
 }
 
 // BirdsInWeek returns the total bird count by summing
 // only the items belonging to the given week.
 func BirdsInWeek(birdsPerDay []int, week int) int {
-	panic("Please implement the BirdsInWeek() function")
+	start := (week - 1) * 7
+	return TotalBirdCount(birdsPerDay[start : start+7])
 }
 
 // FixBirdCountLog returns the bird counts after correcting
 // the bird counts for alternate days.
 func FixBirdCountLog(birdsPerDay []int) []int {
-	panic("Please implement the FixBirdCountLog() function")
+	for index, _ := range birdsPerDay {
+		//fmt.Printf("index: %d \n", index)
+		if index%2 == 0 {
+			birdsPerDay[index]++
+		}
+	}
+	return birdsPerDay
 }
